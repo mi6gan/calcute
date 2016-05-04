c7465237583d37ec530d831f5e14c1e8d72cf402
mi6gan <mi666gan@gmail.com>
2016-05-04 15:18:21 +0700

    added icons loading to initial data and static method for car brand schema

--- commands/load-initial-data.js
+++ commands/load-initial-data.js
@@ -5,10 +5,18 @@ var models = require('../models/feedback.js'),
     promise = require('mpromise');
 
 var data = fs.readFileSync('fixtures/legacy.json'),
-    jsonData = JSON.parse(data);
+    jsonData = JSON.parse(data),
+    icons = {
+        BMW: '/assets/build/images/brand-1.png',
+        Audi: '/assets/build/images/brand-2.png',
+        Ford: '/assets/build/images/brand-3.png',
+        Honda: '/assets/build/images/brand-4.png',
+        Hyundai: '/assets/build/images/brand-5.png',
+        Mazda: '/assets/build/images/brand-6.png'
+    };
 angoose.mongoose.connect('localhost/calcute');
 jsonData.brands.forEach(function(brand) {
-    models.CarBrand.findOneAndUpdate({label: brand.text}, {label: brand.text}, {upsert: true}, function(err, brandDoc) {
+    models.CarBrand.findOneAndUpdate({label: brand.text}, {label: brand.text, icon: icons[brand.text]||null}, {upsert: true}, function(err, brandDoc) {
         console.log('Adding brand ' + brand.text + '\n');
         if(err) {
             throw err;
--- models/feedback.js
+++ models/feedback.js
@@ -1,6 +1,7 @@
 var angoose = require("angoose");
 var mongoose = angoose.getMongoose();
 var carBrandSchema = new mongoose.Schema({
+        icon: {type: String},
         label: {type: String},
     }),
     carSchema = new mongoose.Schema({
@@ -15,8 +16,13 @@ var carBrandSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Car'
         }
-    }),
-    Car = mongoose.model('Car', carSchema),
+    });
+var findSorted = function (query, fields, options) {
+    return this.find(query, fields, options).sort('label');
+};
+carBrandSchema.statics.findSorted = findSorted;
+carSchema.statics.findSorted = findSorted;
+var Car = mongoose.model('Car', carSchema),
     CarBrand = mongoose.model('CarBrand', carBrandSchema),
     Feedback = mongoose.model('Feedback', feedbackSchema);
 exports.Car = Car;
