6961044583036bbda3fe990edc147df673605265
mi6gan <mi666gan@gmail.com>
2016-05-22 04:28:36 +0700

    added static method for year ranges

--- models/public/feedback.js
+++ models/public/feedback.js
@@ -15,13 +15,23 @@ var carBrandSchema = new mongoose.Schema({
         car: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Car'
-        }
+        },
+        year: {type: Number, min: 2000}
     });
 var findSorted = function (query, fields, options) {
     return this.find(query, fields, options).sort('label');
 };
 carBrandSchema.statics.findSorted = findSorted;
 carSchema.statics.findSorted = findSorted;
+feedbackSchema.statics.getYearsRange = function remote($callback) {
+    var min = feedbackSchema.paths.year.options.min,
+        max = (new Date()).getFullYear(),
+        years = Array();
+    for(var i = max; i >= min; i--) {
+        years.push(i);
+    }
+    $callback(null, years);
+};
 var Car = mongoose.model('Car', carSchema),
     CarBrand = mongoose.model('CarBrand', carBrandSchema),
     Feedback = mongoose.model('Feedback', feedbackSchema);
