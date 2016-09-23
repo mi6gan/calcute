8a453c7eea7a90c9b49b4c7a13deb20bfdff306a
mi6gan <mi666gan@gmail.com>
2016-09-24 01:07:20 +0300

    fixed mask input, conditional fields visibility

--- apps/demo.js
+++ apps/demo.js
@@ -21,14 +21,14 @@
         'ngRoute': require('angular-route'),
         'ngAnimate': require('angular-animate')
     };
-    var app = angular.module('Demo', Object.keys(extModules));
+    var app = angular.module('demo', Object.keys(extModules));
     angular.forEach(modules, function(module){
         module(this);
     }, app);
     angular.forEach(directives, function(directive){
         this.directive(directive.name, directive);
     }, app);
-    app.controller('KaskoCalculator', function($scope, $timeout, Feedback, Car, CarBrand, DiscountInfo){
+    app.controller('kaskoCalculator', function($scope, $timeout, Feedback, Car, CarBrand, DiscountInfo){
         /*
         $scope.$watch( 'feedback.car', function (newVal, oldVal, scope) {
             if( newVal && typeof(newVal) != 'object' ) {
@@ -57,10 +57,11 @@
         });
         */
         if(settings.DEBUG){
+            /*
         $scope.feedback = new Feedback({
             car: { fullLabel: 'Custom Car' },
             year: Feedback.prototype.schema.paths.year.options.enum[0],
-            capacity: "21312",
+            capacity: "2131",
             price: Feedback.prototype.schema.paths.price.options.enum[0],
             credit: Feedback.prototype.schema.paths.credit.options.enum[0],
             city: Feedback.prototype.schema.paths.city.options.enum[0],
@@ -68,9 +69,10 @@
             franchise: Feedback.prototype.schema.paths.franchise.options.enum[0],
             franchiseSum: 1000,
             fullName: "ewrwe ewrwer",
-            phoneNumber: "+7 (342) 342 34 23"
+        //    phoneNumber: "+7 (342) 342 34 23"
         });
         $scope.feedbackForm = true;
+        */
         }
         /*
         //$scope.saveFeedback();
