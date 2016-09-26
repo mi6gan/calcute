4fb8cb9c211b140f0d05d84f1ea1f515d123dcee
mi6gan <mi666gan@gmail.com>
2016-09-27 01:04:04 +0300

    finished with frontend for step 2

--- apps/demo.js
+++ apps/demo.js
@@ -21,14 +21,14 @@
         'ngRoute': require('angular-route'),
         'ngAnimate': require('angular-animate')
     };
-    var app = angular.module('demo', Object.keys(extModules));
+    var app = angular.module('appDemo', Object.keys(extModules));
     angular.forEach(modules, function(module){
         module(this);
     }, app);
     angular.forEach(directives, function(directive){
         this.directive(directive.name, directive);
     }, app);
-    app.controller('kaskoCalculator', function($scope, $timeout, Feedback, Car, CarBrand, DiscountInfo){
+    app.controller('KKController', function($scope, $timeout, Feedback, Car, CarBrand, DiscountInfo){
         /*
         $scope.$watch( 'feedback.car', function (newVal, oldVal, scope) {
             if( newVal && typeof(newVal) != 'object' ) {
@@ -49,30 +49,30 @@
         //$scope.feedback = $scope.newFeedback();
         //$scope.discountInfo = new DiscountInfo();
         //$scope.feedback.$submitted = true;  
-        /*
-        $scope.discountInfo = angular.extend(new DiscountInfo(), {
-            fullName: 'name name name',
-            birthDate: '5.11.2017',
-            licenceId: '1234 567890'
-        });
-        */
         if(settings.DEBUG){
-            /*
         $scope.feedback = new Feedback({
             car: { fullLabel: 'Custom Car' },
             year: Feedback.prototype.schema.paths.year.options.enum[0],
-            capacity: "2131",
+            capacity: 213,
             price: Feedback.prototype.schema.paths.price.options.enum[0],
             credit: Feedback.prototype.schema.paths.credit.options.enum[0],
             city: Feedback.prototype.schema.paths.city.options.enum[0],
+            drivers: [],
+            bank: Feedback.prototype.schema.paths.bank.options.enum[0],
             type: [Feedback.prototype.schema.paths.type.options.enum[0]],
             franchise: Feedback.prototype.schema.paths.franchise.options.enum[0],
             franchiseSum: 1000,
             fullName: "ewrwe ewrwer",
-        //    phoneNumber: "+7 (342) 342 34 23"
+            phoneNumber: "+7 (342) 342 34 23"
+        });
+        $scope.discountInfo = new DiscountInfo({
+            birthDate: '5.11.2017',
+            drivers: [],
+            isNotFirst: DiscountInfo.prototype.schema.paths.isNotFirst.options.enum[0],
+            withConsult: DiscountInfo.prototype.schema.paths.withConsult.options.enum[0],
+            tarif: DiscountInfo.prototype.schema.paths.tarif.options.enum[0]
         });
         $scope.feedbackForm = true;
-        */
         }
         /*
         //$scope.saveFeedback();
